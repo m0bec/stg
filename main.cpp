073216143e24c0f4fcc4de6b57a96d642da5f053
mobec <mobec622@outlook.jp>
2016-08-29 23:39:29 +0900

    環境整備
    ＤＸライブラリを使える状態にし、フルスクリーンの画面が表示できるmainを作成

---
+++ main.cpp
@@ -0,0 +1,44 @@
+
+
+#include "DxLib.h"
+
+// WinMain関数
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	// 画面モードの設定
+	SetGraphMode(640, 480, 16);
+
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1) return -1;
+
+	// グラフィックの描画先を裏画面にセット
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	
+
+	// 移動ルーチン
+	while (1)
+	{
+		// 画面を初期化(真っ黒にする)
+		ClearDrawScreen();
+
+		
+
+		// 裏画面の内容を表画面にコピーする
+		ScreenFlip();
+
+
+		// Windows 特有の面倒な処理をＤＸライブラリにやらせる
+		// -1 が返ってきたらループを抜ける
+		if (ProcessMessage() < 0) break;
+
+		// もしＥＳＣキーが押されていたらループから抜ける
+		if (CheckHitKey(KEY_INPUT_ESCAPE)) break;
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;					// ソフトの終了
+}
+
