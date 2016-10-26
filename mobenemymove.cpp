a11c8c2e4c72392f62e71e050b86c487a67ca0ef
mobec <mobec622@outlook.jp>
2016-10-26 16:05:50 +0900

    動きを追加（２，３）

--- mobenemymove.cpp
+++ mobenemymove.cpp
@@ -5,6 +5,14 @@ void bossenemy::straight_down(std::list<enemy_element>::iterator itr) {
 	itr->y += straight_down_speed;
 }
 
+void bossenemy::straight_left(std::list<enemy_element>::iterator itr) {
+	itr->x -= 3;
+}
+
+void bossenemy::straight_right(std::list<enemy_element>::iterator itr) {
+	itr->x += 3;
+}
+
 void bossenemy::allocation_enemymove(std::list<enemy_element>::iterator iterate) {
 	switch (iterate->movenum) {
 	case 0:
@@ -12,6 +20,11 @@ void bossenemy::allocation_enemymove(std::list<enemy_element>::iterator iterate)
 		break;
 
 	case 1:
+		straight_right(iterate);
+		break;
+
+	case 2:
+		straight_left(iterate);
 		break;
 
 	case 99:
